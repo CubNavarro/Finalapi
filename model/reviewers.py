ef65eb852c6c67c0b084a811fb288aa3f516be11
CubNavarro <caleb.evan.navarro@gmail.com>
2023-02-27 14:43:39 -0800

    iojkhbolj

--- model/reviewers.py
+++ model/reviewers.py
@@ -5,7 +5,6 @@ import os, base64
 import json
 
 from __init__ import app, db
-from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
