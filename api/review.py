3d6c572e66314cfd792a4b807abbff3866135f63
CubNavarro <caleb.evan.navarro@gmail.com>
2023-02-28 13:55:38 -0800

    aeknjenw

--- api/review.py
+++ api/review.py
@@ -1,89 +1,57 @@
-import json
 from flask import Blueprint, request, jsonify
-from flask_restful import Api, Resource # used for REST API building
-from datetime import datetime
+from flask_restful import Api, Resource, reqparse # used for REST API building
+# the class ActivityDay, defined in the corresponding model file for the feature, is being imported for its usage in the api.
+from model.review import reviewer
 
-from model.reviewers import reviewadd
-
-# Change variable name and API name and prefix
-review_api = Blueprint('review_api', __name__,
-                   url_prefix='/api/reviews')
+# this is where the blueprint class is defined and the url prefix is set, which is then registered to the app in the main.py file.
+review_api = Blueprint('review_api', __name__, url_prefix='/api/review')
 
 # API docs https://flask-restful.readthedocs.io/en/latest/api.html
 api = Api(review_api)
 
-class ReviewAPI:     
+# this is the main entry point for the app, with the class reviewAPI. 
+class reviewAPI:
+    # the _create class is being referred to for the post method, to post the objects.        
     class _Create(Resource):
         def post(self):
-            ''' Read data for json body '''
-            body = request.get_json()
+             ''' Read data for json body '''
+             body = request.json
+             
+             ''' Avoid garbage in, error checking '''
+            # valiaddress name
             
-            ''' Avoid garbage in, error checking '''
-            # validate name
-            name = body.get('name')
-            if name is None or len(name) < 2:
-                return {'message': f'Name is missing, or is less than 2 characters'}, 400
-            
-            uid = body.get('uid')
-            if uid is None or len(uid) < 2:
-                return {'message': f'User ID is missing, or is less than 2 characters'}, 400
-            # look for password and dob
-            # validate uid
+            # here, this handles error checking, as the shortest review in the world is 3 characters, so if the review is less than that, it is deemed invalid and not added to the DB.
+             name = body.get('name')
+             if name is None or len(name) < 3:
+                return {'message': f'name is missing'}, 210
+           
+            # look for address, fun variables
+             email = body.get('email')
+             reviewtx = body.get('reviewtx')
 
-            email = body.get('')
-            if email is None or len(email) < 0:
-                return {'message': f'email is missing, or is less than 2 characters'}, 210
-            reviewtx = body.get('reviewtx')
-            if reviewtx is None or len(reviewtx) < 1:
-                return {'message': f'review is missing, or is less than 2 characters'}, 210
-            star = body.get('star')
-            if star is None or len(star) < 1:
-                return {'message': f'star is missing, or is less than 2 characters'}, 210
-            
 
-            ''' #1: Key code block, setup USER OBJECT '''
-            uo = reviewadd(name=name, email=email, reviewtx=reviewtx, star=star)
-            
-            ''' Additional garbage error checking '''
-            # set password if provided
-            
-            
-            ''' #2: Key Code block to add user to database '''
-            # create user in database
-            review = uo.create()
-            # success returns json of user
-            if review:
+             # this sets up the review object
+             uo = reviewer(review, address, fun)
+           
+           
+             # this adds the review to the DB (uo.create())
+             review = uo.create()
+             
+             # if the addition was successful, then the review is returned to the user in a readable JSON format.
+             if review:
                 return jsonify(review.read())
             # failure returns error
-            return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 400
-
+             return {'message': f'Processed review error'}, 210
+    
+    # _Read class, needed for the GET request.     
     class _Read(Resource):
         def get(self):
-            users = reviewadd.query.all()    # read/extract all users from database
-            json_ready = [user.read() for user in users]  # prepare output in json
+            review = reviewer.query.all()    # read/extract all review from database
+            json_ready = [review.read() for review in review]  # prepares the readable output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
-    
-    class _Security(Resource):
+        
 
-        def post(self):
-            ''' Read data for json body '''
-            body = request.get_json()
-            
-            ''' Get Data '''
-            uid = body.get('uid')
-            if uid is None or len(uid) < 2:
-                return {'message': f'User ID is missing, or is less than 2 characters'}, 400
-            
-            
-            ''' Find user '''
-            
-            
-            ''' authenticated user '''
-            
 
-            
-
-    # building RESTapi endpoint
+    # building the API endpoints. there is a create and read endpoint, to serve for both the GET and POST requests.
     api.add_resource(_Create, '/create')
     api.add_resource(_Read, '/')
-    api.add_resource(_Security, '/authenticate')
--- model/reviewers.py
+++ model/reviewers.py
@@ -1,6 +1,11 @@
-""" database dependencies to support sqliteDB examples """
+'''
+The below 7 lines import all of the modules necessary for the backend and backend/frontend connection. The especially important imports are the json, init, and sqlalchemy imports.
+The "import json" import allows for the code in line 53, where the dump records are returned in json format, so that the python objects are readable in JSON format (text format). SQLAlchemy
+is the database library being used to store all of the database info for this feature. Finally, the _init_ module is necessary, as it lets the interpreter know that there is Python code in a particular directory. 
+In this case, there is Python code in the /api and /model directories.
+'''
+
 from random import randrange
-from datetime import date
 import os, base64
 import json
 
@@ -8,241 +13,151 @@ from __init__ import app, db
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
-''' Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along '''
-
-# Define the Post class to manage actions in 'posts' table,  with a relationship to 'users' table
-class Update(db.Model):
-    __tablename__ = 'posts'
-
-    # Define the Notes schema
+'''
+The below is where the "reviewDay" class is being defined. This contains all of the data for the feature that needs to be managed.
+'''
+class reviewer(db.Model):
+    __tablereview__ = 'reviewTime'  
+    
+    '''
+    The below sets all of the keys that are going to be looked at. The id key is special, as it is the primary key. This is what any sort of PUT and DELETE requests will be passed through if operable.
+    '''
     id = db.Column(db.Integer, primary_key=True)
-    note = db.Column(db.Text, unique=False, nullable=False)
-    email = db.Column(db.String, unique=False)
-    reviewtx = db.Column(db.String, unique=False)
-    star = db.Column(db.String, unique=False)
-    # Define a relationship in Notes Schema to userID who originates the note, many-to-one (many notes to one user)
-    userID = db.Column(db.Integer, db.ForeignKey('users.id'))
-
-    # Constructor of a Notes object, initializes of instance variables within object
-    def __init__(self, id, note, email, reviewtx, star):
-        self.userID = id
-        self.note = note
-        self.email = email
-        self.reviewtx = reviewtx
-        self.star = star
-
-    # Returns a string representation of the Notes object, similar to java toString()
-    # returns string
-    def __repr__(self):
-        return "Notes(" + str(self.id) + "," + self.note + "," + str(self.userID) + ")"
-
-    # CRUD create, adds a new record to the Notes table
-    # returns the object added or None in case of an error
-    def create(self):
-        try:
-            # creates a Notes object from Notes(db.Model) class, passes initializers
-            db.session.add(self)  # add prepares to persist person object to Notes table
-            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
-            return self
-        except IntegrityError:
-            db.session.remove()
-            return None
-
-    # CRUD read, returns dictionary representation of Notes object
-    # returns dictionary
-    def read(self):
-        # encode image
-        path = app.config['UPLOAD_FOLDER']
-        file = os.path.join(path, self.image)
-        file_text = open(file, 'rb')
-        file_read = file_text.read()
-       # file_encode = base64.encodebytes(file_read)
-        
-        return {
-            "id": self.id,
-            "userID": self.userID,
-            "reviewtx": self.reviewtx,
-            "star": self.star,
-            "email":self.email,
-           # "base64": str(file_encode)
-        }
-
-
-# Define the User class to manage actions in the 'users' table
-# -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
-# -- a.) db.Model is like an inner layer of the onion in ORM
-# -- b.) User represents data we want to store, something that is built on db.Model
-# -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL
-class reviewadd(db.Model):
-    __tablename__ = 'users'  # table name is plural, class name is singular
-
-    # Define the User schema with "vars" from object
-    id = db.Column(db.Integer, primary_key=True)
-    _name = db.Column(db.String(255), unique=False, nullable=False)
-    _uid = db.Column(db.String(255), unique=True, nullable=False)
-    _email = db.Column(db.String(255), unique=False, nullable=True)
-    _star = db.Column(db.String(255), unique=False, nullable=False)
-    _reviewtx = db.Column(db.String(255), unique=False, nullable=False)
-
-    # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
-    posts = db.relationship("Update", cascade='all, delete', backref='users', lazy=True)
-
-    # constructor of a User object, initializes the instance variables within object (self)
-    def __init__(self, name, uid, email, star, reviewtx):
-        self._name = name    # variables with self prefix become part of the object, 
-        self._uid = uid
+    _name = db.Column(db.String(255), nullable=False)
+    _email = db.Column(db.String(255), nullable=False)
+    _reviewtx = db.Column(db.String(255), nullable=False )
+    
+    '''
+    This is constructing the review object and the "_init_" portion is initializing the variables within that review object. 
+    In this case, this is the review, email, and fun variables that are within this object.
+    '''
+    def __init__(self, email, reviewtx):
+        self._name = name
         self._email = email
-        self._star = star
         self._reviewtx = reviewtx
-
-    # a name getter method, extracts name from object
+    
+    '''
+    the following lines 44-75 contain the setter and getter methods. each of the three above variables (review, email, fun)
+    are being extracted from the object and then upemaild after the object is created. 
+    '''
     @property
     def name(self):
         return self._name
     
-    # a setter function, allows name to be updated after initial object creation
+    # setting name variable in object
+
     @name.setter
     def name(self, name):
-        self._name = name
+       self._name = name
     
-    # a getter method, extracts email from object
-    @property
-    def uid(self):
-        return self._uid
-    
-    # a setter function, allows name to be updated after initial object creation
-    @uid.setter
-    def uid(self, uid):
-        self._uid = uid
-        
-    # check if uid parameter matches user id in object, return boolean
-    def is_uid(self, uid):
-        return self._uid == uid
-   
+    # extracting email from object
     @property
     def email(self):
-       return self._email
-  
+        return self._email
+    
+    # setting email variable in object
+    
     @email.setter
     def email(self, email):
        self._email = email
-
-
-    def is_email(self, email):
-       return self._email == email
     
-    @property
-    def star(self):
-       return self._star
-  
-    @star.setter
-    def star(self, star):
-       self._star = star
-
-
-    def is_star(self, star):
-        return self._star == star
+    # extracting fun from object
     
     @property
     def reviewtx(self):
-       return self._reviewtx
-  
-    @reviewtx.setter
-    def reviewtx(self, reviewtx):
-       self._reviewtx = reviewtx
-
-
-    def is_reviewtx(self, reviewtx):
-       return self._reviewtx == reviewtx
-  
-
+        return self._reviewtx
     
-    # output content using str(object) in human readable form, uses getter
-    # output content using json dumps, this is ready for API response
+    # setting fun variable in object
+    
+    @reviewtx.setter
+    def fun(self, reviewtx):
+       self._reviewtx = reviewtx
+    
+    '''
+    The content is being outputted using "str(self)". It is being returned in JSON format, which is a readable format. This is a getter function.
+    '''
     def __str__(self):
         return json.dumps(self.read())
-
-    # CRUD create/add a new record to the table
-    # returns self or None on error
+    
+    
+    '''
+    defining the create method. self allows us to access all of the attributes 
+    of the current object. after the create method is defined, the data is queried from the DB.
+    in this case, since it is the create method, the data is being ADDED, and then db.session.commit() is used
+    to commit the DB transaction and apply the change to the DB.
+    '''
+    
+    '''
+    here, there is an integrity error "except" statement. db.session would be autocommitted 
+    without the db.session.remove() line, and that's something we don't want for the purpose of the project.
+    '''
     def create(self):
         try:
-            # creates a person object from User(db.Model) class, passes initializers
-            db.session.add(self)  # add prepares to persist person object to Users table
-            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
+            db.session.add(self)  
+            db.session.commit() 
             return self
         except IntegrityError:
             db.session.remove()
             return None
+    
+    '''
+    the delete method is defined with the "self" parameter. this method is mainly for certain instances in the DB being 
+    garbage collected, and the object kills itself.
+    '''
 
-    # CRUD read converts self to dictionary
-    # returns dictionary
-    def read(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "uid": self.uid,
-            "email": self.email,
-            "star": self.star,
-            "reviewtx": self.reviewtx,
-          #  "posts": [post.read() for post in self.posts]
-        }
-
-    # CRUD update: updates user name, password, phone
-    # returns self
-    def update(self, name="", uid="", email="", star="", reviewtx=""):
-        """only updates values with length"""
-        if len(name) > 0:
-            self.name = name
-        if len(uid) > 0:
-            self.uid = uid
-        if len(email) > 0:
-           self.email = email
-        if len (star) > 0:
-           self.star = star 
-        if len(reviewtx) > 0:
-           self.reviewtx = reviewtx    
-        db.session.commit()
-        return self
-
-    # CRUD delete: remove self
-    # None
     def delete(self):
         db.session.delete(self)
         db.session.commit()
         return None
 
+    '''
+    read method with the self parameter, reading the object with all of the 
+    properties: review, email, and fun are being returned.
+    '''
+    def read(self):
+        return {
+            "name" : self.name,
+            "email" : self.email,
+            "reviewtx" : self.reviewtx,
+        }
 
-"""Database Creation and Testing """
-
-
-# Builds working data for testing
+'''
+handling the situation where the table is completely empty,
+returns the length from the session query of the initialized class reviewDay to be 0.
+'''
+def review_table_empty():
+    return len(db.session.query(reviewer).all()) == 0
+'''
+defines the initreview function, and then creates the tables and the DB here through the db.create_all() method.
+'''
 def initreview():
-    with app.app_context():
-        """Create database and tables"""
-        db.init_app(app)
-        db.create_all()
-        
-        """Tester data for table"""
-        r1 = reviewadd(name='Sam Johnson', uid='r1', email= 'Sam.johnson@gmail.com', star='three', reviewtx='The place was amazing and I really enjoyed it')
-        r2 = reviewadd(name='Eric Middleton', uid='r2', email='Eric.Middleton@gmail.om', star='five', reviewtx='I wanted to stay longer and I really loved it. Thanks for the amazing Vac')
-        r3 = reviewadd(name='Theo Fatless', uid='r3', email='Theo.Fatless@gmail.com', star='two', reviewtx='It was really nice but it was kinda hard to look though the website')
-        r4 = reviewadd(name='Crunchy Mcfee ', uid='r4', email='Crunchy.Mcfee@gmail.com', star='three', reviewtx='It was good but I wanted a little more')
-        r5 = reviewadd(name='Thicc Boi', uid='r5', email='Thicc.Boi@gmail.com', star='five', reviewtx='It satisfided me and I had a fun time')
+    db.create_all()
+    #db.init_app(app)
+    if not review_table_empty():
+        return
+    
+    r1 = reviewer('Daves Hot Chicken', '1268 Auto Park Way, Escondido, CA 92029', "8/10")
+    r2 = reviewer('Raising Canes', '8223 Mira Mesa Blvd, San Diego, CA 92126', "10/10")
+    r3 = reviewer('Belmont Park', '3146 Mission Blvd, San Diego, CA 92109', "7/10")
+    r4 = reviewer('Potato Chip Rock', 'Ramona, CA 92065', "6/10") 
+    
+    '''
+    the variable "reviewslist" being used for the tester data, containing a1, a2, a3, and a4 the variables with the sample data above.
+    '''
+    reviewslist = [r1, r2, r3, r4]
+    
+    
+    '''
+    the below is for the sample data: for each review in the defined reviewlist, the DB session will add that review, and then commit the transaction
+    with the next line. or, if there is bad/duplicate data, the data will not be committed, and session will be rolled back to its previous
+    state. 
+    '''
 
-        users = [r1, r2, r3, r4, r5]
-
-        """Builds sample user/note(s) data"""
-        for user in users:
-            try:
-                '''add a few 1 to 4 notes per user'''
-                for num in range(randrange(1, 4)):
-                     note = "#### " + user.name + " note " + str(num) + ". \n Generated by test data."
-                user.posts.append(Update(id=user.id, note=note, email=user._email, star=user._star, reviewtx=user._reviewtx, image='ncs_logo.png'))
-                '''add user/post data to table'''
-                user.create()
-            except IntegrityError:
-                '''fails with bad or duplicate data'''
-                db.session.remove()
-                print(f"Records exist, duplicate email, or error: {user.uid}")
-            
+    for review in reviewslist:
+        try:
+            db.session.add(review)
+            db.session.commit()
+        except IntegrityError as e:
+            print("Error: " +str(e))
+            '''fails with bad or duplicate data'''
+            db.session.rollback()    
